ff1e53c59dca84e38e1770535b4aae1c3fbefc83
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-07-26 22:05:25 +0900

    チャートのテスト描画

---
+++ components/Chart.vue
@@ -0,0 +1,28 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  data() {
+    return {
+      chartData: {
+        labels: ['January', 'February'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 20],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  },
+}
+</script>
--- pages/trial.vue
+++ pages/trial.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <p>hogehoge</p>
+    <h2>チャートのテスト描画</h2>
+    <chart />
   </div>
 </template>
+
+<script>
+import Chart from '~/components/Chart.vue'
+
+export default {
+  components: {
+    Chart,
+  },
+}
+</script>
