52499326e1a0be0c7d5270bbcef1582a7131d777
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-08-10 21:00:26 +0900

    積み上げ棒グラフ設定

--- components/Chart.vue
+++ components/Chart.vue
@@ -1,23 +1,57 @@
 <script>
-import { Line } from 'vue-chartjs'
+import { Bar } from 'vue-chartjs'
 
 export default {
-  extends: Line,
+  extends: Bar,
   data() {
     return {
       chartData: {
-        labels: ['January', 'February'],
+        labels: ['運用状況', '調達状況'],
         datasets: [
           {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 20],
+            type: 'bar',
+            barPercentage: 1.2,
+            label: '資産',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            data: [100, null],
+          },
+          {
+            type: 'bar',
+            barPercentage: 1.2,
+            label: '純資産',
+            data: [null, 30],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+          },
+          {
+            type: 'bar',
+            barPercentage: 1.2,
+            label: '負債',
+            data: [null, 70],
+            backgroundColor: 'rgba(255, 206, 86, 0.2)',
+            borderColor: 'rgba(255, 206, 86, 1)',
+            borderWidth: 1,
           },
         ],
       },
       options: {
         responsive: true,
         maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              stacked: true,
+            },
+          ],
+          yAxes: [
+            {
+              stacked: true,
+            },
+          ],
+        },
       },
     }
   },
