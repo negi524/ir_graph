46b6ee2362e14fd1055dad41f7a9cae343f86c66
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-08-22 01:00:00 +0900

    チャートのデータをリアクティブにできるように修正

--- components/BalanceSheet.vue
+++ components/BalanceSheet.vue
@@ -1,28 +1,14 @@
 <script>
-import { Bar } from 'vue-chartjs'
+import { Bar, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
 
 export default {
   extends: Bar,
+  mixins: [reactiveProp],
   props: {
-    // 資産
-    assets: {
-      type: Number,
-      default: 0,
-    },
-    // 負債
-    liabilities: {
-      type: Number,
-      default: 0,
-    },
-    // 純資産
-    netAssets: {
-      type: Number,
-      default: 0,
-    },
-  },
-  data() {
-    return {
-      chartData: {
+    chartData: {
+      type: Object,
+      default: () => ({
         labels: ['運用状況', '調達状況'],
         datasets: [
           {
@@ -32,13 +18,13 @@ export default {
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgba(255,99,132,1)',
             borderWidth: 1,
-            data: [this.assets, null],
+            data: [0, null],
           },
           {
             type: 'bar',
             barPercentage: 1.2,
             label: '純資産',
-            data: [null, this.netAssets],
+            data: [null, 0],
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgba(54, 162, 235, 1)',
             borderWidth: 1,
@@ -47,14 +33,18 @@ export default {
             type: 'bar',
             barPercentage: 1.2,
             label: '負債',
-            data: [null, this.liabilities],
+            data: [null, 0],
             backgroundColor: 'rgba(255, 206, 86, 0.2)',
             borderColor: 'rgba(255, 206, 86, 1)',
             borderWidth: 1,
           },
         ],
-      },
-      options: {
+      }),
+    },
+    options: {
+      type: Object,
+      requied: false,
+      default: () => ({
         responsive: true,
         maintainAspectRatio: false,
         scales: {
@@ -69,8 +59,8 @@ export default {
             },
           ],
         },
-      },
-    }
+      }),
+    },
   },
   mounted() {
     this.renderChart(this.chartData, this.options)
--- pages/trial.vue
+++ pages/trial.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>チャートのテスト描画</h2>
-    <balance-sheet v-bind="bsData" />
+    <balance-sheet :chart-data="datacollection" />
   </div>
 </template>
 
@@ -14,15 +14,63 @@ export default {
   },
   data() {
     return {
-      bsData: {
-        // 資産
-        assets: 100,
-        // 負債
-        liabilities: 30,
-        // 純資産
-        netAssets: 70,
+      datacollection: null,
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              stacked: true,
+            },
+          ],
+          yAxes: [
+            {
+              stacked: true,
+            },
+          ],
+        },
       },
     }
+  },
+  mounted() {
+    this.fillData()
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: ['運用状況', '調達状況'],
+        datasets: [
+          {
+            type: 'bar',
+            barPercentage: 1.2,
+            label: '資産',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            data: [100, null],
+          },
+          {
+            type: 'bar',
+            barPercentage: 1.2,
+            label: '純資産',
+            data: [null, 30],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+          },
+          {
+            type: 'bar',
+            barPercentage: 1.2,
+            label: '負債',
+            data: [null, 70],
+            backgroundColor: 'rgba(255, 206, 86, 0.2)',
+            borderColor: 'rgba(255, 206, 86, 1)',
+            borderWidth: 1,
+          },
+        ],
+      }
+    },
   },
 }
 </script>
