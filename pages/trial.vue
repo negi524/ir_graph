9d4c55c258b85ac32ceeeee0f4669dec6a7fa2e0
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-08-22 01:20:05 +0900

    外部の入力を受け付けるように修正

--- pages/trial.vue
+++ pages/trial.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    <h2>チャートのテスト描画</h2>
+  <div class="container">
+    <h2 class="mt-5">バランスシート</h2>
+    <div class="row my-5 mx-4">
+      <div class="col-3"><label>資産</label></div>
+      <div class="col-9">
+        <b-form-input v-model="assets" placeholder="資産" />
+      </div>
+      <div class="col-3"><label>負債</label></div>
+      <div class="col-9">
+        <b-form-input v-model="liabilities" placeholder="負債" />
+      </div>
+      <div class="col-3"><label>純資産</label></div>
+      <div class="col-9">
+        <b-form-input v-model="netAssets" placeholder="純資産" />
+      </div>
+      <b-button
+        class="mt-4 mx-auto"
+        variant="outline-primary"
+        @click="fillData()"
+        >反映
+      </b-button>
+    </div>
     <balance-sheet :chart-data="datacollection" />
   </div>
 </template>
@@ -14,23 +34,14 @@ export default {
   },
   data() {
     return {
+      // チャートを構成するデータセット一覧
       datacollection: null,
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          xAxes: [
-            {
-              stacked: true,
-            },
-          ],
-          yAxes: [
-            {
-              stacked: true,
-            },
-          ],
-        },
-      },
+      // 資産
+      assets: 100,
+      // 負債
+      liabilities: 30,
+      // 純資産
+      netAssets: 70,
     }
   },
   mounted() {
@@ -48,13 +59,13 @@ export default {
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgba(255,99,132,1)',
             borderWidth: 1,
-            data: [100, null],
+            data: [this.assets, null],
           },
           {
             type: 'bar',
             barPercentage: 1.2,
             label: '純資産',
-            data: [null, 30],
+            data: [null, this.netAssets],
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgba(54, 162, 235, 1)',
             borderWidth: 1,
@@ -63,7 +74,7 @@ export default {
             type: 'bar',
             barPercentage: 1.2,
             label: '負債',
-            data: [null, 70],
+            data: [null, this.liabilities],
             backgroundColor: 'rgba(255, 206, 86, 0.2)',
             borderColor: 'rgba(255, 206, 86, 1)',
             borderWidth: 1,
