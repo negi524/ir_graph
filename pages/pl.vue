87b5a00d2947495e1194e21b768ce4dcd03d1445
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-08-22 12:23:24 +0900

    損益計算書のグラフ作成

---
+++ pages/pl.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h2 class="mt-5">損益計算書</h2>
+    <profit-and-loss />
+  </div>
+</template>
+
+<script>
+import ProfitAndLoss from '~/components/ProfitAndLoss.vue'
+
+export default {
+  components: {
+    ProfitAndLoss,
+  },
+  data() {
+    return {
+      // チャートを構成するデータセット一覧
+      datacollection: null,
+    }
+  },
+  mounted() {
+    this.fillData()
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {}
+    },
+  },
+}
+</script>
