a66c3170bde799afce5cdf511232919d947c6c51
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-08-22 12:31:01 +0900

    損益計算書にデータ入れられるように修正

--- pages/pl.vue
+++ pages/pl.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h2 class="mt-5">損益計算書（P/L）</h2>
-    <profit-and-loss />
+    <profit-and-loss :chart-data="datacollection" />
   </div>
 </template>
 
@@ -23,7 +23,47 @@ export default {
   },
   methods: {
     fillData() {
-      this.datacollection = {}
+      this.datacollection = {
+        labels: ['借方', '貸方'],
+        datasets: [
+          {
+            type: 'bar',
+            barPercentage: 1.2,
+            label: '利益',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            data: [80, null],
+          },
+          {
+            type: 'bar',
+            barPercentage: 1.2,
+            label: '費用',
+            data: [20, null],
+            backgroundColor: 'rgba(255, 159, 64, 0.2)',
+            borderColor: 'rgba(255, 159, 64, 1)',
+            borderWidth: 1,
+          },
+          {
+            type: 'bar',
+            barPercentage: 1.2,
+            label: '損失',
+            data: [null, 60],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+          },
+          {
+            type: 'bar',
+            barPercentage: 1.2,
+            label: '収益',
+            data: [null, 10],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+          },
+        ],
+      }
     },
   },
 }
