b37eb50823ef2d82e7a099d61696a6a5e06eaae5
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-08-25 09:41:12 +0900

    トップページにリンク追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,16 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        ir_graph
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <ul class="nav mt-5">
+      <li class="nav-item">
+        <nuxt-link to="/bs" class="nav-link active">貸借対照表</nuxt-link>
+      </li>
+      <li class="nav-item">
+        <nuxt-link to="/pl" class="nav-link active">損益計算書</nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
