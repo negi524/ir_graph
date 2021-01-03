c1311ff18ccaabc49030ee4c0f4d856fb5bcfa15
negi524 <egtnrumpbiizarre.info@gmail.com>
2021-01-03 15:45:07 +0900

    propsで受け取ったデータからキャッシュフロー計算書のグラフ描画

--- components/CacheFlow.vue
+++ components/CacheFlow.vue
@@ -1,8 +1,36 @@
 <script>
-import { Bar } from 'vue-chartjs'
+import { Bar, mixins } from 'vue-chartjs'
+const { reactiveData } = mixins
 
 export default {
   extends: Bar,
+  mixins: [reactiveData],
+  props: {
+    // 期首現金
+    beginningCache: {
+      type: Number,
+      required: true,
+      default: 100,
+    },
+    // 営業活動
+    operatingActivities: {
+      type: Number,
+      required: true,
+      default: 40,
+    },
+    // 投資活動
+    investmentActivities: {
+      type: Number,
+      required: true,
+      default: 60,
+    },
+    // 財務活動
+    financingActivities: {
+      type: Number,
+      required: true,
+      default: 30,
+    },
+  },
   data() {
     return {
       // プラスの場合の色定義
@@ -38,6 +66,37 @@ export default {
       },
     }
   },
+  computed: {
+    /**
+     * 営業活動のグラフ描画に必要な隠しグラフ
+     * 期首現金がそのまま返却される
+     */
+    hiddenOperatingAmount() {
+      return this.beginningCache
+    },
+    /**
+     * 投資活動のグラフ描画に必要な隠しグラフ
+     * 「期首現金 + 営業活動」が返却される
+     */
+    hiddenInvestmentAmount() {
+      return this.beginningCache + this.operatingActivities
+    },
+    /**
+     * 財務活動のグラフ描画に必要な隠しグラフ
+     * 「期首現金 + 営業活動 + 投資活動」が返却される
+     */
+    hiddenFinancingAmount() {
+      return this.beginningCache + this.operatingActivities + this.investmentActivities
+    },
+    /**
+     * 期末現金の結果を返却する
+     */
+    endCache() {
+      return (
+        this.beginningCache + this.operatingActivities + this.investmentActivities + this.financingActivities
+      )
+    },
+  },
   mounted() {
     this.fillData()
     this.renderChart(this.chartData, this.options)
@@ -45,35 +104,59 @@ export default {
   methods: {
     fillData() {
       this.chartData = {
-        labels: ['期首現金', '営業活動', '投資活動', '期末現金'],
+        labels: ['期首現金', '営業活動', '投資活動', '財務活動', '期末現金'],
         datasets: [
           {
             label: '期首現金',
-            data: [700, null, null, null],
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgba(54, 162, 235, 1)',
+            data: [this.beginningCache, null, null, null, null],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
           },
           {
             label: '営業活動の非表示要素',
-            data: [null, 700, null, null],
+            data: [null, this.hiddenOperatingAmount, null, null, null],
+            // 配色は透明にする
             backgroundColor: 'rgba(0, 0, 0, 0)',
             borderColor: 'rgba(0, 0, 0, 0)',
           },
           {
             label: '営業活動',
-            data: [null, 550, null, null],
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgba(54, 162, 235, 1)',
+            data: [null, this.operatingActivities, null, null, null],
+            backgroundColor:
+              this.operatingActivities > 0 ? this.plusColor.background : this.minusColor.background,
+            borderColor: this.operatingActivities > 0 ? this.plusColor.border : this.minusColor.border,
           },
           {
             label: '投資活動の非表示要素',
-            data: [null, null, 200, null],
+            data: [null, null, this.hiddenInvestmentAmount, null, null],
+            // 配色は透明にする
             backgroundColor: 'rgba(0, 0, 0, 0)',
             borderColor: 'rgba(0, 0, 0, 0)',
           },
           {
             label: '投資活動',
-            data: [null, null, 200, null],
+            data: [null, null, this.investmentActivities, null, null],
+            backgroundColor:
+              this.investmentActivities > 0 ? this.plusColor.background : this.minusColor.background,
+            borderColor: this.investmentActivities > 0 ? this.plusColor.border : this.minusColor.border,
+          },
+          {
+            label: '財務活動の非表示要素',
+            data: [null, null, null, this.hiddenFinancingAmount, null],
+            // 配色は透明にする
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            borderColor: 'rgba(0, 0, 0, 0)',
+          },
+          {
+            label: '財務活動',
+            data: [null, null, null, this.financingActivities, null],
+            backgroundColor:
+              this.financingActivities > 0 ? this.plusColor.background : this.minusColor.background,
+            borderColor: this.financingActivities > 0 ? this.plusColor.border : this.minusColor.border,
+          },
+          {
+            label: '期末現金',
+            data: [null, null, null, null, this.endCache],
             backgroundColor: 'rgba(75, 192, 192, 0.2)',
             borderColor: 'rgba(75, 192, 192, 1)',
           },
--- pages/cf.vue
+++ pages/cf.vue
@@ -2,13 +2,17 @@
   <div class="container">
     <global-header />
     <h2 class="mt-5">キャッシュフロー計算書</h2>
-    <p>工事中…</p>
     <ul>
       <li>営業活動</li>
       <li>投資活動</li>
       <li>財務活動</li>
     </ul>
-    <cache-flow />
+    <cache-flow
+      :beginning-cache="beginningCache"
+      :operating-activities="operatingActivities"
+      :investment-activities="investmentActivities"
+      :financing-activities="financingActivities"
+    />
     <global-footer />
   </div>
 </template>
@@ -27,12 +31,13 @@ export default {
   data() {
     return {
       // 期首現金
+      beginningCache: 100,
       // 営業活動
-      operatingActivities: 0,
+      operatingActivities: 40,
       // 投資活動
-      investmentActivities: 0,
+      investmentActivities: 60,
       // 財務活動
-      financingActivities: 0,
+      financingActivities: 30,
       // 期末現金
     }
   },
