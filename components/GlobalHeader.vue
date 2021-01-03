e8a9989b6a29ad36502dd4429a319edcfc4d24e1
negi524 <egtnrumpbiizarre.info@gmail.com>
2021-01-03 14:13:47 +0900

    chartjsのウォーターフォール用プラグインのインストール

--- components/GlobalHeader.vue
+++ components/GlobalHeader.vue
@@ -7,6 +7,9 @@
       <li class="nav-item">
         <nuxt-link to="/pl" class="nav-link active">損益計算書</nuxt-link>
       </li>
+      <li class="nav-item">
+        <nuxt-link to="/cf" class="nav-link active">キャッシュフロー計算書</nuxt-link>
+      </li>
     </ul>
   </header>
 </template>
