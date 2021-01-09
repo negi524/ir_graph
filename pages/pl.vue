f83966831c7e002ecc572aedbf04384b821018db
negi524 <egtnrumpbiizarre.info@gmail.com>
2021-01-09 22:11:39 +0900

    損益計算書のクエリパラメータ追加

--- pages/pl.vue
+++ pages/pl.vue
@@ -2,6 +2,20 @@
   <div class="container">
     <global-header />
     <h2 class="mt-5">損益計算書（P/L）</h2>
+    <table v-if="hasQueryParams" class="table table-bordered">
+      <thead>
+        <tr>
+          <th scope="col">企業名</th>
+          <th scope="col">時期</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{ corpName }}</td>
+          <td>{{ quarter }}</td>
+        </tr>
+      </tbody>
+    </table>
     <div class="row mt-4">
       <!-- グラフ -->
       <profit-and-loss
@@ -157,6 +171,38 @@ export default {
         netLoss: 0,
       },
     }
+  },
+  computed: {
+    /**
+     * リクエストパラメータに設定された企業名を取得する
+     */
+    corpName() {
+      if (this.$route.query) {
+        return this.$route.query.name ? this.$route.query.name : null
+      } else {
+        return null
+      }
+    },
+    /**
+     * リクエストパラメータに設定された時期を取得する
+     */
+    quarter() {
+      if (this.$route.query) {
+        return this.$route.query.quarter ? this.$route.query.quarter : null
+      } else {
+        return null
+      }
+    },
+    /**
+     * クエリパラメータがあればtrue
+     */
+    hasQueryParams() {
+      if (!this.$route.query) {
+        return false
+      } else {
+        return this.$route.query.name || this.$route.query.quarter
+      }
+    },
   },
   created() {
     // ローカルストレージにデータが存在すれば取り出してセットする
